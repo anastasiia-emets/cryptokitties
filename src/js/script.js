125ec7e86aab4ebfdc0d4de60b2a96d085c3bd8e
Anastasiia Yemets <40547968+anastasiia-emets@users.noreply.github.com>
2019-01-14 20:34:57 +0200

    Add files via upload

--- src/js/script.js
+++ src/js/script.js
@@ -9,10 +9,14 @@
 //style="background-color: ${random_bg_color()}"
 //style="background-color: ${catColor}">${cat.name}"
 function getCat(cat) {
+  const hashNumber = Math.abs(hashCode(cat.name));
+  const arrayNumber = hashNumber % colors.length;
+  const catColor = colors[arrayNumber];
+
   return `
 <div class="wrapp1">
         <div class="card-cat">
-            <div class="card-cat-container" >
+            <div class="card-cat-container" style="background-color: ${catColor}">
                 <div class="card-cat-container__price">for sale = ${
     cat.price
     }</div>
@@ -30,6 +34,7 @@ function getCat(cat) {
 };
 
 
+
 function getcats(cats) {
   return cats.map(cat => getCat(cat))
     .join('');
@@ -39,14 +44,15 @@ function getcats(cats) {
 let loader = document.getElementById('loader');
 loader.classList.add('loader_opened');
 fetch('https://ma-cats-api.herokuapp.com/api/cats?&per_page=12')
+  .then((response) => {
+    return response.json();
+  })
   .then(
-    function (response) {
-      response.json().then(function (cats) {
-        setTimeout(function () {
-          document.querySelector('.wrapp').insertAdjacentHTML("afterbegin", getcats(cats.cats));
-          loader.classList.remove('loader_opened');
-        }, 2000);
-      });
+    function (cats) {
+      setTimeout(function () {
+        document.querySelector('.wrapp').insertAdjacentHTML("afterbegin", getcats(cats.cats));
+        loader.classList.remove('loader_opened');
+      }, 2000);
     }
   );
 
@@ -70,14 +76,6 @@ const colors = [
   'blue',
   'green'
 ]
-
-document.querySelector('body').innerHTML = cats().map((cat) => {
-  const hashNumber = Math.abs(hashCode(cat.name));
-  const arrayNumber = hashNumber % colors.length;
-  const catColor = colors[arrayNumber];
-  
-  return `<div class="card-cat-container" style="background-color: ${catColor}">${cat.name}</div>`
-}).join('');
 
 
 
