981c22a1ddade691c614b43bc42fb229778fe48a
Anastasiia Yemets <40547968+anastasiia-emets@users.noreply.github.com>
2019-01-30 13:34:14 +0200

    Add files via upload

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,224 +1,232 @@
+class Kitten {
+  constructor(cat) {
+    this.cat = cat;
+    this.colors = [
+      "#D3E8FF", "#FDE9E4", "#CDF5D4", "#EFE1DA", "#ECF4E0", "#FAEEFA", "#EEE9E8", "#D9F5CB", "#DFDFFA", "#FAF4CF"
+    ]
+  }
+  getMarkup() {
+    const cat = this.cat;
+    const hashNumber = Math.abs(this._hashCode(cat.name));
+    const arrayNumber = hashNumber % this.colors.length;
+    const catColor = this.colors[arrayNumber];
+    const auction = cat.auction;
+    const exactPrice = auction.current_price / 100000000000000000;
+    let price;
 
-function getCat(cat) {
-  const hashNumber = Math.abs(hashCode(cat.name));
-  const arrayNumber = hashNumber % colors.length;
-  const catColor = colors[arrayNumber];
-  let imageAdress = cat.image_url;
-  let catName;
-  if (cat.name){
-      catName = cat.name;
-  } else {catName = "- - - - -"}
-  let catId = cat.id;
-  let catCategory = function () {
-      let status = cat.status.cooldown_index;
-      if(status < 2){return "Fast"}
-      else if (status < 3){return "Swift"}
-      else  if (status < 5){return "Snappy"}
-      else  if (status < 7){return "Brisk"}
-      else  if (status < 10){return "Plodding"}
-      else  if (status < 11){return "Slow"}
-      else  if (status < 13){return "Sluggish"}
-      else  if (status >= 13){return "Catatonic"}
-  };
- 
+    if (exactPrice.toString().charAt(4) !== '') {
+      price = exactPrice.toFixed(3);
+    } else {
+      price = exactPrice.toFixed(2);
+    }
+    let cooldown = cat.status.cooldown_index;
+    let category;
+    if (cooldown < 2) {
+      category = 'Fast';
+    } else if (cooldown < 3) {
+      category = 'Swift';
+    } else if (cooldown < 5) {
+      category = 'Snappy';
+    } else if (cooldown < 7) {
+      category = 'Brisk';
+    } else if (cooldown < 10) {
+      category = 'Plodding';
+    } else if (cooldown < 11) {
+      category = 'Slow';
+    } else if (cooldown < 13) {
+      category = 'Sluggish';
+    } else if (cooldown >= 13) {
+      category = 'Catatonic';
+    }
 
-
-  return `
+    if (cat.name === null) {
+      cat.name = '---';
+    }
+   
+    return `
 <div class="wrapp1">
         <div class="card-cat">
             <div class="card-cat-container" style="background-color: ${catColor}">
-                <div class="card-cat-container__price">for sale = ${
-    cat.price
-    }</div>
+                <div class="card-cat-container__price">for sale = ${price}
+      </div>
                 <img class="card-container-cat__image-cat" alt="cat" src="${
-    cat.img_url
-    }">
+      cat.image_url
+      }">
             </div>
             <div class="card-cat__id">id #${cat.id}</div>
             <div class="card-cat__name">name ${cat.name}</div>
-            <div class="card-cat__category">category ${cat.category}</div>
+            <div class="card-cat__category">category ${category}</div>
             <div class="cat-add-to-cart">
               <a rel="import" href="import/../cat.html" class="button see-more">More Details</a>
-              <a href="#0" class="button add-to-cart" data-id=${cat.id}>Add to Cart</a>
+              <button class="button add-to-cart" data-id=${cat.id}>Add to Cart</button>
             </div>
         </div>
 </div>
         `;
+  }
+  _hashCode(stringValue) {
+    var hash = 0, i, chr;
+    if (!stringValue || stringValue.length === 0) return hash;
+    for (i = 0; i < stringValue.length; i++) {
+      chr = stringValue.charCodeAt(i);
+      hash = ((hash << 5) - hash) + chr;
+      hash |= 0; // Convert to 32bit integer
+    }
+    return hash;
+  };
+}
+class KittenList {
+  constructor(cats) {
+    this.cats = cats;
 
-};
+    document.addEventListener('click', (e) => {
+      if (e.target.matches('.add-to-cart')) {
+        card.add(/* Find cat by id */)
+      }
+    });
+  }
+  getKittenMarkup(cats) {
+    return this.cats.map(cat => {
+
+      const kitten = new Kitten(cat);
 
 
 
-function getcats(cats) {
-  return cats.map(cat => getCat(cat))
-    .join('');
+
+      return kitten.getMarkup();
+    })
+      .join('');
+  }
+  
+
+}
+class ShoppingCart {
+  constructor() {
+
+  }
+  // addToCart(id) {
+
+  //   var obj = products[id];
+  //   if (productsInCart.length === 0 || productFound(obj.id) === undefined) {
+  //     productsInCart.push({ product: obj, quantity: 1 });
+  //   } else {
+  //     productsInCart.forEach(function (item) {
+  //       if (item.product.id === obj.id) {
+  //         item.quantity++;
+  //       }
+  //     });
+  //   }
+  //   generateCartList();
+
+
+  // }
+  // setupListeners() {
+
+  //   productsEl.addEventListener("click", function (event) {
+  //     var el = event.target;
+  //     if (el.classList.contains("add-to-cart")) {
+  //       var elId = el.dataset.id;
+  //       addToCart(elId);
+  //     }
+  //   });
+
+  //   emptyCartEl.addEventListener("click", function (event) {
+  //     if (confirm("Are you sure?")) {
+  //       productsInCart = [];
+  //     }
+  //     generateCartList();
+  //   });
+  // }
+
+
 }
 
-document.addEventListener("DOMContentLoaded", async function (event) {
+
+
+
+let pageOffset = 0;
+let pageInclude = "sale";
+let pageOrderBy = "current_price";
+let pageOrderDirection = "asc";
+let pageSearch = "";
+
+async function loading_page() {
+  let pageLimit = 12;
   let loader = document.getElementById('loader');
   loader.classList.add('loader_opened');
-  const response = await fetch('https://ma-cats-api.herokuapp.com/api/cats?&per_page=12');
 
+  const response = await fetch(`https://api.cryptokitties.co/v2/kitties?offset=${pageOffset}&limit=${pageLimit}${pageSearch}&parents=false&authenticated=false&include=${pageInclude}&orderBy=${pageOrderBy}&orderDirection=${pageOrderDirection}&total=true`);
   const cats = await response.json();
- 
+
+  const kittenlist = new KittenList(cats.kitties);
+
+
+
   setTimeout(function () {
-    document.querySelector('.wrapp').insertAdjacentHTML("afterbegin", getcats(cats.cats));
+    document.querySelector('.wrapp').insertAdjacentHTML("afterbegin", kittenlist.getKittenMarkup());
     loader.classList.remove('loader_opened');
   }, 2000);
 }
-);
 
-
-
-
-
-function hashCode(stringValue) {
-  var hash = 0, i, chr;
-  if (stringValue.length === 0) return hash;
-  for (i = 0; i < stringValue.length; i++) {
-    chr = stringValue.charCodeAt(i);
-    hash = ((hash << 5) - hash) + chr;
-    hash |= 0; // Convert to 32bit integer
-  }
-  return hash;
-};
-
-const colors = [
-  "#D3E8FF", "#FDE9E4", "#CDF5D4", "#EFE1DA", "#ECF4E0", "#FAEEFA", "#EEE9E8", "#D9F5CB", "#DFDFFA", "#FAF4CF"
-]
-
-
-
-
-
-document.querySelector(".cats-options__search input").addEventListener("focus", function () {
-  
-  document.querySelector(".cats-options__search-options").classList.toggle("hidden");
-});
-
-document.addEventListener("click", function (e) {
-  if(e.target.closest(".cats-options")) return;
- 
-});
-
-var Cart = (function($) {
-  "use strict";
-  
-  
-  var catsEl = document.querySelector(".wrapp"),
-      cartEl =     document.querySelector(".shopping-cart-list"),
-      catsQuantityEl = document.querySelector(".cats-quantity"),
-      emptyCartEl = document.querySelector(".empty-cart-btn"),
-      cartCheckoutEl = document.querySelector(".cart-checkout"),
-      totalPriceEl = document.querySelector(".total-price");
-  
- 
-   var cats=getcats();
-      var catsInCart = [];
-  
-  
-  var generateCartList = function() {
-    
-    cartEl.innerHTML = "";
-    
-    catsInCart.forEach(function(item) {
-      var li = document.createElement("li");
-      li.innerHTML = `${item.quantity} ${item.cat.name} - $${item.cat.price * item.quantity}`;
-      cartEl.appendChild(li);
-    });
-    
-    catsQuantityEl.innerHTML = catsInCart.length;
-    
-    generateCartButtons()
-  }
-  
-  
- 
-  var generateCartButtons = function() {
-    if(catsInCart.length > 0) {
-      emptyCartEl.style.display = "block";
-      cartCheckoutEl.style.display = "block"
-      totalPriceEl.innerHTML = "$ " + calculateTotalPrice();
-    } else {
-      emptyCartEl.style.display = "none";
-      cartCheckoutEl.style.display = "none";
-    }
-  }
-  
-  
-  var setupListeners = function() {
-    catsEl.addEventListener("click", function(event) {
-      var el = event.target;
-      if(el.classList.contains("add-to-cart")) {
-       var elId = el.dataset.id;
-       addToCart(elId);
-      }
-    });
-    
-    emptyCartEl.addEventListener("click", function(event) {
-      if(confirm("Are you sure?")) {
-        catsInCart = [];
-      }
-      generateCartList();
-    });
-  }
-  
-  
-  var addToCart = function(id) {
-    var obj = cats[id];
-    if(catsInCart.length === 0 || catFound(obj.id) === undefined) {
-      catsInCart.push({cat: obj, quantity: 1});
-    } else {
-      catsInCart.forEach(function(item) {
-        if(item.cat.id === obj.id) {
-          item.quantity++;
-        }
-      });
-    }
-    generateCartList();
-  }
-  
-  
-  
-  var catFound = function(catId) {
-    return catsInCart.find(function(item) {
-      return item.cat.id === catId;
-    });
-  }
-
-  var calculateTotalPrice = function() {
-    return catsInCart.reduce(function(total, item) {
-      return total + (item.cat.price *  item.quantity);
-    }, 0);
-  }
-  
-
-  var init = function() {
-    getcats(cats);
-    setupListeners();
-  }
-  
-  return {
-    init: init
-  };
-  
- 
-})(jQuery);
-
-Cart.init();
-function nextPage() {
-  document.querySelector(".page-navigation__prev").classList.remove("disabled");
+document.addEventListener("DOMContentLoaded", loading_page);
+[...document.getElementsByClassName("button_next")].forEach(button => button.addEventListener('click', () => {
   pageOffset += 12;
-  document.querySelector(".loader").classList.toggle("hidden");
-  getCats(`https://api.cryptokitties.co/v2/kitties?offset=${pageOffset}&limit=${pageLimit}&parents=false&authenticated=false&include=${pageInclude}${pageSearch}&orderBy=${pageOrderBy}&orderDirection=${pageOrderDirection}&total=true`);
-}
 
-function prevPage() {
+  loading_page();
+}));
+
+[...document.getElementsByClassName("button_prev")].forEach(button => button.addEventListener('click', () => {
+
   if (pageOffset === 0) return;
   pageOffset -= 12;
-  if (pageOffset === 0){
-      document.querySelector(".page-navigation__prev").classList.add("disabled");
+  loading_page();
+}));
+
+document.querySelector('#search').addEventListener('submit', (event) => {
+  event.preventDefault();
+  const searchValue = document.querySelector('[name="search"]').value;
+  var filteredCats;
+
+  if (searchValue !== null) {
+    filteredCats = list.filter((cat) => {
+      return cat.name && (cat.name.toUpperCase().indexOf(searchValue.toUpperCase())) >= 0;
+    });
+  } else {
+    filteredCats = list;
   }
-  document.querySelector(".loader").classList.toggle("hidden");
-  getCats(`https://api.cryptokitties.co/v2/kitties?offset=${pageOffset}&limit=${pageLimit}&parents=false&authenticated=false&include=${pageInclude}${pageSearch}&orderBy=${pageOrderBy}&orderDirection=${pageOrderDirection}&total=true`);
-}
+  renderCats(filteredCats);
+});
+
+// function renderCats(cats) {
+//   document.querySelector('.wrapp').innerHTML =
+//     this.cats.map(cat => {
+// console.log(cats);
+//       return `
+// <div class="wrapp1">
+//         <div class="card-cat">
+//             <div class="card-cat-container" style="background-color: ${catColor}">
+//                 <div class="card-cat-container__price">for sale = ${
+//         cat.price
+//         }</div>
+//                 <img class="card-container-cat__image-cat" alt="cat" src="${
+//         cat.image_url
+//         }">
+//             </div>
+//             <div class="card-cat__id">id #${cat.id}</div>
+//             <div class="card-cat__name">name ${cat.name}</div>
+//             <div class="card-cat__category">category ${cat.category}</div>
+//             <div class="cat-add-to-cart">
+//               <a rel="import" href="import/../cat.html" class="button see-more">More Details</a>
+//               <button class="button add-to-cart" data-id=${cat.id}>Add to Cart</button>
+//             </div>
+//         </div>
+// </div>
+//         ` })
+//       .join('');
+
+// }
+
+
+
+
+
